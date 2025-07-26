f9c1369b5d8d8f0a8e5b9ad7081aab699bd212b5
niloy <chowdhuryniloy819@gmail.com>
2025-07-27 02:42:14 +0600

    learne3d python user input and if-else

--- practice.py
+++ practice.py
@@ -1,24 +1,40 @@
 from math import sqrt,pow
 
 #print('Hello world');
-x=5;
-x+=10;
-b=x;
-c=2+3j;
-#print(x);
-nums=[12,25,30,50,12];
-dic={1:'niloy',2:'adit',3:'ankon'};
-print(id(x));
-print(id(b));
-x+=1;
-#print(id(x));
-#print(type(c));
+# x=5;
+# x+=10;
+# b=x;
+# c=2+3j;
+# #print(x);
+# nums=[12,25,30,50,12];
+# dic={1:'niloy',2:'adit',3:'ankon'};
+# print(id(x));
+# print(id(b));
+# x+=1;
+# #print(id(x));
+# #print(type(c));
 
-p=25;
-pp=sqrt(p);
-print(pp);
+# p=25;
+# pp=sqrt(p);
+# print(pp);
 
 # del dic[1];
 #print(dic);
 #print(dic.get(1));
 # print(nums);
+
+x=(input('Enter 1st number '));
+y=(input('Enter 2nd number '));
+
+age=int(input('enter your age: '));
+
+if age<20:
+    print('Teenage');
+elif age>=20 and age<45:
+ print('Young');
+
+else:
+    print('Old');
+
+z=x+y;
+#print(z);
