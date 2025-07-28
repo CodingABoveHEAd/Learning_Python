14808b0042208bcdd94b8a379b98e5eb240ec94b
niloy <chowdhuryniloy819@gmail.com>
2025-07-28 21:13:38 +0600

    learned python module

--- practice.py
+++ practice.py
@@ -1,14 +1,7 @@
-def div(a,b):
-    print(a/b)
+from demo import *
 
-def smart_div(func):
-    def inner(a,b):
-        if a<b:
-            a,b=b,a
-        return func(a,b)
+a=int(input('Enter first number: '));
+b=int(input('Enter second number: '));
 
-    return inner
-
-div=smart_div(div)
-
-div(2,4)
+res=div(a,b);
+print(res);
