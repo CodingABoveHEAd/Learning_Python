899e5c714f76dd0ff9e2225ecdd9bd381e579ef5
niloy <chowdhuryniloy819@gmail.com>
2025-07-28 21:04:25 +0600

    learned decorators

--- practice.py
+++ practice.py
@@ -1,12 +1,14 @@
-# f = lambda a,b : a*b;
-# print(f(5,6));
-from functools import reduce
+def div(a,b):
+    print(a/b)
 
-nums=[3,4,5,54,2];
+def smart_div(func):
+    def inner(a,b):
+        if a<b:
+            a,b=b,a
+        return func(a,b)
 
-res1=list(filter(lambda n: n%2==0 ,nums));
-print(res1);
-res=list(map( lambda n : n*2  ,res1));
-print(res);
-res=(reduce( lambda a,b : a+b  ,res));
-print(res);
+    return inner
+
+div=smart_div(div)
+
+div(2,4)
