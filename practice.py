a04eb23bed5f8204766bb555ccb8bd7041927908
niloy <chowdhuryniloy819@gmail.com>
2025-07-28 11:23:05 +0600

    fibonacchi number using python

--- practice.py
+++ practice.py
@@ -1,11 +1,23 @@
-a=10
-b=20
+# a=10
+# b=20
 
-def varcheck() :
-    global a,b;
-    a=15
-    b=25;
-    print('inside: ' ,a,b);
+# def varcheck() :
+#     global a,b;
+#     a=15
+#     b=25;
+#     print('inside: ' ,a,b);
 
-varcheck();
-print('outside: ' , a,b);
+# varcheck();
+# print('outside: ' , a,b);
+
+f0=0;
+f1=1;
+
+n=int(input('Enter the range: '));
+
+print(f0,end=" ");
+for i in range (n):
+    f2=f0+f1;
+    f0=f1;
+    f1=f2;
+    print(f0,end=" ");
